0fc332ad55fe64b3ef1d1af7dcdaf4243ced5ecd
bach9w <78446671+bach9w@users.noreply.github.com>
2023-12-14 18:57:21 +0200

    Update cell-action.tsx

--- app/(dashboard)/[storeId]/(routes)/orders/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/cell-action.tsx
@@ -7,7 +7,7 @@ import {
 	DropdownMenuLabel,
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { BillboardColumn } from './columns';
+import { OrderColumn } from './columns';
 import { Button } from '@/components/ui/button';
 import { Copy, Edit, MoreHorizontal, Trash } from 'lucide-react';
 import toast from 'react-hot-toast';
@@ -18,7 +18,7 @@ import axios from 'axios';
 import { AlertModal } from '@/components/modals/alert-modal';
 
 interface CellActionProps {
-	data: BillboardColumn;
+	data: OrderColumn;
 }
 
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
